e0e0688de2b86281356e47eba7df7dc56b888eca
igora <igoralbuquerque102@hotmail.com>
2026-02-18 16:50:06 -0300

    fix: :bug: Now it works with magic, but has an inconsistent internal state error.
    It allows equipping spells without animation, and it's possible to attack with the spell immediately when in combat with a spell. The problem is that when skipping to switch from weapon to spell or spell to weapon, the internal state of the graph is not updated, making it impossible to attack.

--- src/hook.cpp
+++ src/hook.cpp
@@ -3,68 +3,93 @@
 #include "util.h"
 using namespace Util;
 
-void EquipHook::OnEquipItemPC(RE::PlayerCharacter* a_this, bool a_playAnim)
+void EquipHook::OnEquipItemPC(RE::PlayerCharacter *a_this, bool a_playAnim)
 {
     _OnEquipItemPC(a_this, !SkipAnim(a_this, a_playAnim));
 }
+
 /*
 void EquipHook::OnEquipItemNPC(RE::Actor* a_this, bool a_playAnim)
 {
     _OnEquipItemNPC(a_this, !SkipAnim(a_this, a_playAnim));
 }
 */
-bool CheckIsValidBoundObject(const RE::TESForm* a_object)
+
+bool CheckIsValidBoundObject(const RE::TESForm *a_object)
 {
+    if (!a_object)
+        return false;
 #ifdef WORK_WITH_MAGIC_OBJECTS
-    if ((a_object && a_object->IsMagicItem()))
-        return = true;
-#endif
-    return {//  && (uint32_t)a_object->As<RE::TESObjectWEAP>()->GetWeaponType() <= 6u)
-        a_object && (a_object->As<RE::TESObjectWEAP>() || a_object->As<RE::TESObjectARMO>())
-    };
-}
-void SendEquipEvents(RE::Actor* a_this, RE::TESForm* a_lHandObject, RE::TESForm* a_rHandObject)
-{
-    if (auto animationEventTracker = AnimationEventTracker::GetSingleton()) {
-        animationEventTracker->SendAnimationEvent(a_this, "weaponDraw");
-#ifdef WORK_WITH_MAGIC_OBJECTS
-        if ((a_lHandObject && a_lHandObject->IsMagicItem()) || (a_rHandObject && a_rHandObject->IsMagicItem()))
-            animationEventTracker->SendAnimationEvent(a_this, "MagicEquip_Out");
+    if (a_object->IsMagicItem())
+        return true;
 #endif
 
-        if ((a_lHandObject && (a_lHandObject->IsWeapon() || a_lHandObject->IsArmor()))
-         || (a_rHandObject && (a_rHandObject->IsWeapon() || a_rHandObject->IsArmor()))) {
-            animationEventTracker->SendAnimationEvent(a_this, "WeapEquip_Out");
-            animationEventTracker->SendAnimationEvent(a_this, "WeapEquip_OutMoving");
-        }
+    return a_object->As<RE::TESObjectWEAP>() != nullptr || a_object->As<RE::TESObjectARMO>() != nullptr; //  && (uint32_t)a_object->As<RE::TESObjectWEAP>()->GetWeaponType() <= 6u)
+}
+
+void SendEquipEvents(RE::Actor *a_this, RE::TESForm *a_lHandObject, RE::TESForm *a_rHandObject, bool changePose)
+{
+    if (!a_this || !AnimationEventTracker::GetSingleton())
+        return;
+
+    auto *tracker = AnimationEventTracker::GetSingleton();
+
+    bool rIsSpell = a_rHandObject && a_rHandObject->IsMagicItem();
+    bool lIsSpell = a_lHandObject && a_lHandObject->IsMagicItem();
+    bool rIsWeapon = a_rHandObject && (a_rHandObject->IsWeapon() || a_rHandObject->IsArmor());
+    bool lIsWeapon = a_lHandObject && (a_lHandObject->IsWeapon() || a_lHandObject->IsArmor());
+
+    bool hasSpell = lIsSpell || rIsSpell;
+    bool hasWeapon = lIsWeapon || rIsWeapon;
+
+    tracker->SendAnimationEvent(a_this, "weaponDraw");
+#ifdef WORK_WITH_MAGIC_OBJECTS
+    if (hasSpell)
+    {
+        tracker->SendAnimationEvent(a_this, "Magic_Equip_Out");
+        tracker->SendAnimationEvent(a_this, "Magic_Equip_OutMoving");
+    }
+#endif
+    if (hasWeapon)
+    {
+        tracker->SendAnimationEvent(a_this, "WeapEquip_Out");
+        tracker->SendAnimationEvent(a_this, "WeapEquip_OutMoving");
     }
 }
-bool EquipHook::SkipAnim(RE::PlayerCharacter* a_this, bool a_playAnim)
+
+bool EquipHook::SkipAnim(RE::PlayerCharacter *a_this, bool a_playAnim)
 {
-    _skipAnim = !a_playAnim;
-    if (AnimationEventTracker::GetSingleton()->Register() && a_this && a_this->AsActorState() && a_this->AsActorState()->IsWeaponDrawn()) {
+    bool skipAnim = !a_playAnim;
+    if (AnimationEventTracker::GetSingleton()->Register() && a_this && a_this->AsActorState() && a_this->AsActorState()->IsWeaponDrawn())
+    {
         auto rHandObj = a_this->GetEquippedObject(false);
         auto lHandObj = a_this->GetEquippedObject(true);
 
-        if (!(lHandObj && rHandObj) || CheckIsValidBoundObject(lHandObj) || CheckIsValidBoundObject(rHandObj)) {
+        if (!(lHandObj && rHandObj) || CheckIsValidBoundObject(lHandObj) || CheckIsValidBoundObject(rHandObj))
+        {
             int delay = 300;
             bool skip3D = false;
-            a_this->GetGraphVariableBool("SkipEquipAnimation", _skipAnim);
+            bool changePose = false;
+            a_this->GetGraphVariableBool("SkipEquipAnimation", skipAnim);
             a_this->GetGraphVariableInt("LoadBoundObjectDelay", delay);
             a_this->GetGraphVariableBool("Skip3DLoading", skip3D);
-            if (delay < (int)(*g_deltaTimeRealTime * 1000.f)) delay = (int)(*g_deltaTimeRealTime * 1000.f);   // the loading process will start next frame.
-            if (!skip3D && _skipAnim) {
-                std::jthread delayedEquipThread([=]() {
-                        if (_skipAnim) {
-                            spdlog::debug("equip anim skipped");
-                            std::this_thread::sleep_for(std::chrono::milliseconds(delay));
-                            SendEquipEvents(a_this, lHandObj, rHandObj);
-                            spdlog::debug("weapon 3d model called");
-                        }
-                    }
-                );
+            a_this->GetGraphVariableBool("ChangePose", changePose);
+            if (delay < (int)(*g_deltaTimeRealTime * 1000.f))
+                delay = (int)(*g_deltaTimeRealTime * 1000.f); // the loading process will start next frame.
+            if (!skip3D && skipAnim)
+            {
+                std::jthread delayedEquipThread([=]()
+                                                {
+                    if (skipAnim) {
+                        spdlog::debug("equip anim skipped");
+                        std::this_thread::sleep_for(std::chrono::milliseconds(delay));
+                        SendEquipEvents(a_this, lHandObj, rHandObj, changePose);
+                        spdlog::debug("weapon 3d model called");
+                    } });
                 delayedEquipThread.detach();
             }
         }
-    } return _skipAnim;
+    }
+    _skipAnim = skipAnim;
+    return skipAnim;
 }
